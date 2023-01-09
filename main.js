774fb3ab47c3b4ee7934f0923695b499d3d1e1b7
Naza <nazarenomadero2017@gmail.com>
2023-01-09 18:16:42 -0300

    Parte de JSON

---
+++ main.js
@@ -0,0 +1,31 @@
+
+
+
+
+/*
+       CONVERTIR UN ARREGLO EN UN JSON
+const moto = '{ "marca": "kawasaki", "modelo": 2000, "velocidadm": 120 }';
+
+console.log(JSON.parse(moto));
+*/
+
+
+//      DESIFRADO DE UN JSON
+//   const moto = {
+//     marca: 'kawasaki',
+//     modelo: 2000,
+//     velocidadm: 120,
+//   };
+  
+//   console.log(JSON.stringify(moto));
+
+
+/*  CREACION DE UN JSON 
+    const moto = {
+        marca: 'kawasaki',
+        modelo: 2000,
+        velocidadm: 120
+    };
+
+    console.log(moto);
+    */
